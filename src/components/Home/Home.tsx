b03942c874f85a199356a3212cdc0f1bde6c7c09
SerhiiHerasymiuk <@DESKTOP-8DFR7S5>
2023-05-15 21:41:45 +0300

    Added Reasons

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Home.css";
 import { Banner } from "./Banner/Banner";
+import { Reasons } from "./Reasons/Reasons";
 
 export function Home() {
   return (
     <div className="Home">
       <Banner></Banner>
+      <Reasons></Reasons>
     </div>
   );
 }
