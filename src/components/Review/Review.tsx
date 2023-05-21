24a9962dafc04efc1ea3a38f1895b9cc9b6589dd
SerhiiHerasymiuk <@DESKTOP-8DFR7S5>
2023-05-21 23:40:08 +0300

    Added Examples

--- src/components/Review/Review.tsx
+++ src/components/Review/Review.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Review.css";
 import { ReviewBanner } from "./ReviewBanner/ReviewBanner";
+import { Examples } from "./Examples/Examples";
 
 export function Review() {
   return (
     <div className="Review">
       <ReviewBanner></ReviewBanner>
+      <Examples></Examples>
     </div>
   );
 }
