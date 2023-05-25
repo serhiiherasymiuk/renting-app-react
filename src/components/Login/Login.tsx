1fde47c1baa4470a1af4b14ed5de978f42634398
SerhiiHerasymiuk <@DESKTOP-8DFR7S5>
2023-05-25 22:48:32 +0300

    Added LoginBanner

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import login_banner from "../../materials/login_banner.png";
+import { LoginBanner } from "./LoginBanner/LoginBanner";
+
+export function Login() {
+  return (
+    <div className="Login">
+      <LoginBanner></LoginBanner>
+    </div>
+  );
+}
